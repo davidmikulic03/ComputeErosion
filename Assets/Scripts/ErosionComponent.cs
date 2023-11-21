5be8cbfc094c5dabc2481197f539fb0d2c7ade17
davidmikulic03 <d.mikulic03@gmail.com>
2023-11-21 20:39:08 +0100

    Lot of work

--- Assets/Scripts/ErosionComponent.cs
+++ Assets/Scripts/ErosionComponent.cs
@@ -1,24 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 
 public class ErosionComponent : MonoBehaviour
 {
+    [SerializeField] ComputeShader erosionShader;
 
+    [Header("Perlin Noise")] 
+    [SerializeField] [Range(0.1f, 10f)] float perlinScale = 2;
+    [SerializeField][Range(1, 16)] int perlinResolution = 4;
+    [SerializeField] Vector2Int outputResolution = new Vector2Int(4096, 4096);
+    [Tooltip("in km")][SerializeField] float size;
+    [Tooltip("in km")][SerializeField] float height;
+
+    [FormerlySerializedAs("powerBySlope")]
+    [Header("Simulation")] 
+    [SerializeField] [Range(0.5f, 10f)] float slopePower = 1;
 
     [SerializeField] bool runErosion = false;
+
+    private RenderTexture generatedHeight;
+    private RenderTexture generatedNormal;
+    private RenderTexture generatedAlbedo;
+    private MeshRenderer meshRenderer;
 
     private void OnValidate()
     {
         if (runErosion)
         {
+            double currentTime = Time.timeAsDouble;
             Erode();
+            Debug.Log(1000 * (Time.timeAsDouble - currentTime) + "ms");
+            runErosion = false;
         }
     }
 
     void Erode()
     {
+        InitializeErosion();
+        int kernel = erosionShader.FindKernel("CSMain");
+        uint x, y, z;
+        erosionShader.GetKernelThreadGroupSizes(kernel, out x, out y, out z);
+
+        erosionShader.Dispatch(kernel, outputResolution.x / (int)x, outputResolution.y / (int)y, 1);
+
+        meshRenderer.sharedMaterial.SetTexture("_Albedo", generatedAlbedo);
+        meshRenderer.sharedMaterial.SetTexture("_Normal", generatedNormal);
+        meshRenderer.sharedMaterial.SetTexture("_DisplacementMap", generatedHeight);
+        
+    }
+
+    void InitializeErosion()
+    {
+        generatedAlbedo.Release();
+        generatedHeight.Release();
+        generatedNormal.Release();
+        
+        generatedAlbedo = new RenderTexture(outputResolution.x, outputResolution.y, 0);
+        generatedHeight = new RenderTexture(outputResolution.x, outputResolution.y, 0, RenderTextureFormat.RFloat);
+        generatedNormal = new RenderTexture(outputResolution.x, outputResolution.y, 0);
+        generatedHeight.enableRandomWrite = true;
+        generatedAlbedo.enableRandomWrite = true;
+        generatedNormal.enableRandomWrite = true;
+        //generatedNormal.
+        
+        generatedHeight.Create();
+        generatedAlbedo.Create();
+        generatedNormal.Create();
+        
+        erosionShader.SetTexture(0, "Albedo", generatedAlbedo);
+        erosionShader.SetTexture(0, "Displacement", generatedHeight);
+        erosionShader.SetTexture(0, "Normal", generatedNormal);
+        
+        erosionShader.SetFloat("PerlinScale", perlinScale);
+        erosionShader.SetInt("PerlinDepth", perlinResolution);
+        
+        erosionShader.SetVector("Resolution", new Vector2(outputResolution.x, outputResolution.y));
+        erosionShader.SetFloat("Size", size);
+        erosionShader.SetFloat("Height", this.height);
+        erosionShader.SetFloat("SlopePower", slopePower);
+        
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial.SetFloat("_Displacement", height);
+        meshRenderer.sharedMaterial.SetFloat("_Size", size);
 
     }
 }
--- Assets/Scripts/Terrain.shader
+++ Assets/Scripts/Terrain.shader
@@ -3,6 +3,8 @@ Shader "Unlit/Terrain"
     Properties
     {
         _DisplacementMap ("Displacement Map", 2D) = "white" {}
+        _Albedo ("Albedo", 2D) = "white" {}
+        _Normal ("Normal", 2D) = "white" {}
         _Displacement ("Displacement", Float) = 1
     }
     SubShader
@@ -33,19 +35,27 @@ Shader "Unlit/Terrain"
                 float2 uv : TEXCOORD0;
                 UNITY_FOG_COORDS(1)
                 float4 vertex : SV_POSITION;
+                float3 normal : NORMAL;
             };
 
             sampler2D _DisplacementMap;
-            float4 _DisplacementMap_TexelSize;
             float4 _DisplacementMap_ST;
-            float _Displacement;
-            
 
+            sampler2D   _Normal;
+            float4      _Normal_ST;
+            
+            sampler2D _Albedo;
+            float4 _Albedo_ST;
+
+            float _Displacement;
+            float _Size;
+            
             v2f vert (appdata v)
             {
                 v2f o;
                 float d = tex2Dlod(_DisplacementMap, v.uv);
-                v.vertex.xyz += v.normal.xyz * d * _Displacement;
+                v.vertex.xyz += v.normal.xyz * d * _Displacement / _Size;
+                o.normal = (tex2Dlod(_Normal, v.uv) * 2 - 1).xzy;
 
                 o.uv = TRANSFORM_TEX(v.uv, _DisplacementMap);
                 o.vertex = UnityObjectToClipPos(v.vertex);
@@ -55,18 +65,15 @@ Shader "Unlit/Terrain"
 
             float4 frag (v2f i) : SV_Target
             {
-                i.uv = (uint2)(i.uv * _DisplacementMap_TexelSize.zw) / _DisplacementMap_TexelSize.zw;
-                float4 col = tex2D(_DisplacementMap, i.uv);
-                float4 slope = 100 * float4(
-                    col.x - tex2D(_DisplacementMap, i.uv + _DisplacementMap_TexelSize.x).x, 
-                    col.x - tex2D(_DisplacementMap, i.uv - _DisplacementMap_TexelSize.x).x,
-                    col.x - tex2D(_DisplacementMap, i.uv + _DisplacementMap_TexelSize.y).x, 
-                    col.x - tex2D(_DisplacementMap, i.uv - _DisplacementMap_TexelSize.y).x
-                    );
+                float4 col = tex2D(_Albedo, i.uv)  * float4(2, 2, 1, 0) - float4(1, 1, 0, 0);
+                float3 normal = tex2D(_Normal, i.uv).xyz * 2 - 1;
+                //col = dot(normal, -normalize(float3(2, 1.0, 4.0)));
+                //col = 1.0 - dot(normal, float3(0, 0, 1));
 
-
+                return col.z;
+                return float4(col.xy, 0, 0);
+                return float4(normal.xy, 0, 1);
                 UNITY_APPLY_FOG(i.fogCoord, col);
-                return slope;
             }
             ENDCG
         }
