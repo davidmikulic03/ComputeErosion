58c4567b00e45ec64aa9bd8b26e2bad4e3e1130a
davidmikulic03 <d.mikulic03@gmail.com>
2023-11-22 18:24:30 +0100

    Progress

--- Assets/Scripts/ErosionComponent.cs
+++ Assets/Scripts/ErosionComponent.cs
@@ -12,65 +12,84 @@ public class ErosionComponent : MonoBehaviour
     [SerializeField] Vector2Int outputResolution = new Vector2Int(4096, 4096);
     [Tooltip("in km")][SerializeField] float size;
     [Tooltip("in km")][SerializeField] float height;
-
-    [FormerlySerializedAs("powerBySlope")]
+    
     [Header("Simulation")] 
-    [SerializeField] [Range(0.5f, 10f)] float slopePower = 1;
+    [SerializeField][Range(0.5f, 10f)] float slopePower = 1;
 
-    [SerializeField] bool runErosion = false;
+    [Tooltip("Number of droplets per pixel")][SerializeField] [Range(0.1f, 10.0f)] float dropletDensity = 1.0f;
+    [SerializeField] [Range(10, 1000)] int maxDropletSteps = 100;
+    [SerializeField] [Range(0.0001f, 0.01f)] float dropletWeight = 0.001f;
+
+    [SerializeField] bool erode = false;
 
     private RenderTexture generatedHeight;
     private RenderTexture generatedNormal;
     private RenderTexture generatedAlbedo;
+    private RenderTexture generatedSlope;
+    private RenderTexture generatedErosion;
     private MeshRenderer meshRenderer;
 
     private void OnValidate()
     {
-        if (runErosion)
+        if (erode)
         {
-            double currentTime = Time.timeAsDouble;
+            Initialize();
             Erode();
-            Debug.Log(1000 * (Time.timeAsDouble - currentTime) + "ms");
-            runErosion = false;
+            erode = false;
         }
     }
 
     void Erode()
     {
-        InitializeErosion();
-        int kernel = erosionShader.FindKernel("CSMain");
-        uint x, y, z;
-        erosionShader.GetKernelThreadGroupSizes(kernel, out x, out y, out z);
-
-        erosionShader.Dispatch(kernel, outputResolution.x / (int)x, outputResolution.y / (int)y, 1);
-
-        meshRenderer.sharedMaterial.SetTexture("_Albedo", generatedAlbedo);
-        meshRenderer.sharedMaterial.SetTexture("_Normal", generatedNormal);
-        meshRenderer.sharedMaterial.SetTexture("_DisplacementMap", generatedHeight);
-        
+        int traceKernel = erosionShader.FindKernel("CSTrace");
+        erosionShader.GetKernelThreadGroupSizes(traceKernel, out uint x, out uint y, out uint z);
+        erosionShader.Dispatch(traceKernel, (int)(outputResolution.x * dropletDensity / x), (int)(outputResolution.x * dropletDensity / y), 1);
     }
 
     void InitializeErosion()
     {
-        generatedAlbedo.Release();
-        generatedHeight.Release();
-        generatedNormal.Release();
+        if(generatedAlbedo)
+            generatedAlbedo.Release();
+        if (generatedHeight)
+            generatedHeight.Release();
+        if (generatedNormal)
+            generatedNormal.Release();
+        if (generatedSlope)
+            generatedSlope.Release();
+        if (generatedErosion)
+            generatedErosion.Release();
         
         generatedAlbedo = new RenderTexture(outputResolution.x, outputResolution.y, 0);
         generatedHeight = new RenderTexture(outputResolution.x, outputResolution.y, 0, RenderTextureFormat.RFloat);
         generatedNormal = new RenderTexture(outputResolution.x, outputResolution.y, 0);
+        generatedSlope = new RenderTexture(outputResolution.x, outputResolution.y, 0, RenderTextureFormat.RFloat);
+        generatedErosion = new RenderTexture(outputResolution.x, outputResolution.y, 0, RenderTextureFormat.RFloat);
         generatedHeight.enableRandomWrite = true;
         generatedAlbedo.enableRandomWrite = true;
         generatedNormal.enableRandomWrite = true;
-        //generatedNormal.
+        generatedSlope.enableRandomWrite = true;
+        generatedErosion.enableRandomWrite = true;
         
         generatedHeight.Create();
         generatedAlbedo.Create();
         generatedNormal.Create();
+        generatedSlope.Create();
         
         erosionShader.SetTexture(0, "Albedo", generatedAlbedo);
         erosionShader.SetTexture(0, "Displacement", generatedHeight);
         erosionShader.SetTexture(0, "Normal", generatedNormal);
+        erosionShader.SetTexture(0, "Slope", generatedSlope);
+        erosionShader.SetTexture(0, "Erosion", generatedErosion);
+        
+        erosionShader.SetTexture(1, "Albedo", generatedAlbedo);
+        erosionShader.SetTexture(1, "Displacement", generatedHeight);
+        erosionShader.SetTexture(1, "Normal", generatedNormal);
+        erosionShader.SetTexture(1, "Slope", generatedSlope);
+        erosionShader.SetTexture(1, "Erosion", generatedErosion);
+        
+        erosionShader.SetTexture(2, "Displacement", generatedHeight);
+        erosionShader.SetTexture(2, "Normal", generatedNormal);
+        erosionShader.SetTexture(2, "Slope", generatedSlope);
         
         erosionShader.SetFloat("PerlinScale", perlinScale);
         erosionShader.SetInt("PerlinDepth", perlinResolution);
@@ -79,10 +98,31 @@ public class ErosionComponent : MonoBehaviour
         erosionShader.SetFloat("Size", size);
         erosionShader.SetFloat("Height", this.height);
         erosionShader.SetFloat("SlopePower", slopePower);
+        erosionShader.SetFloat("DropletDensity", dropletDensity);
+        erosionShader.SetInt("MaxDropletSteps", maxDropletSteps);
         
         meshRenderer = GetComponent<MeshRenderer>();
         meshRenderer.sharedMaterial.SetFloat("_Displacement", height);
         meshRenderer.sharedMaterial.SetFloat("_Size", size);
 
+    }
+
+    void Initialize()
+    {
+        InitializeErosion();
+        int initializeKernel = erosionShader.FindKernel("GenerateNoise");
+        erosionShader.GetKernelThreadGroupSizes(initializeKernel, out uint ix, out uint iy, out uint iz);
+        erosionShader.Dispatch(initializeKernel, outputResolution.x / (int)ix, outputResolution.y / (int)iy, 1);
+        
+        int normalsKernel = erosionShader.FindKernel("GenerateNormals");
+        erosionShader.GetKernelThreadGroupSizes(initializeKernel, out uint x, out uint y, out uint z);
+        erosionShader.Dispatch(normalsKernel, outputResolution.x / (int)x, outputResolution.y / (int)y, 1);
+        
+        
+        meshRenderer.sharedMaterial.SetTexture("_Albedo", generatedAlbedo);
+        meshRenderer.sharedMaterial.SetTexture("_Normal", generatedNormal);
+        meshRenderer.sharedMaterial.SetTexture("_DisplacementMap", generatedHeight);
+        meshRenderer.sharedMaterial.SetTexture("_Slope", generatedSlope);
+        meshRenderer.sharedMaterial.SetTexture("_Erosion", generatedErosion);
     }
 }
--- Assets/Scripts/Terrain.shader
+++ Assets/Scripts/Terrain.shader
@@ -5,15 +5,20 @@ Shader "Unlit/Terrain"
         _DisplacementMap ("Displacement Map", 2D) = "white" {}
         _Albedo ("Albedo", 2D) = "white" {}
         _Normal ("Normal", 2D) = "white" {}
+        _Slope ("Slope", 2D) = "white" {}
+        _Erosion ("Erosion Map", 2D) = "white" {}
         _Displacement ("Displacement", Float) = 1
+        _DisplaceAlongNormal("Displace Along Normal", float) = 0
     }
     SubShader
     {
         Tags { "RenderType"="Opaque" }
         LOD 100
-
+        
+        
         Pass
         {
+            
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
@@ -47,16 +52,25 @@ Shader "Unlit/Terrain"
             sampler2D _Albedo;
             float4 _Albedo_ST;
 
+            sampler2D _Slope;
+            float4 _Slope_ST;
+
+            sampler2D _Erosion;
+            float4 _Erosion_ST;
+
             float _Displacement;
             float _Size;
+
+            float _DisplaceAlongNormal;
             
             v2f vert (appdata v)
             {
                 v2f o;
                 float d = tex2Dlod(_DisplacementMap, v.uv);
                 v.vertex.xyz += v.normal.xyz * d * _Displacement / _Size;
-                o.normal = (tex2Dlod(_Normal, v.uv) * 2 - 1).xzy;
-
+                v.normal.xyz = tex2Dlod(_Normal, v.uv).xzy * 2 - 1;
+                v.vertex.xyz += float3(v.normal.x, 0, v.normal.y) * _DisplaceAlongNormal;
+                
                 o.uv = TRANSFORM_TEX(v.uv, _DisplacementMap);
                 o.vertex = UnityObjectToClipPos(v.vertex);
                 UNITY_TRANSFER_FOG(o,o.vertex);
@@ -65,14 +79,16 @@ Shader "Unlit/Terrain"
 
             float4 frag (v2f i) : SV_Target
             {
-                float4 col = tex2D(_Albedo, i.uv)  * float4(2, 2, 1, 0) - float4(1, 1, 0, 0);
+                float4 col = tex2D(_Albedo, i.uv);
+                float slope = tex2D(_Slope, i.uv);
                 float3 normal = tex2D(_Normal, i.uv).xyz * 2 - 1;
-                //col = dot(normal, -normalize(float3(2, 1.0, 4.0)));
-                //col = 1.0 - dot(normal, float3(0, 0, 1));
+                float erosionMap = tex2D(_Erosion, i.uv);
 
-                return col.z;
-                return float4(col.xy, 0, 0);
-                return float4(normal.xy, 0, 1);
+                return erosionMap;
+                return dot(normal, normalize(float3(1, 2, 4)));
+                return float4(normal.xyz, 1);
+                return normal.z;
+                return slope;
                 UNITY_APPLY_FOG(i.fogCoord, col);
             }
             ENDCG
